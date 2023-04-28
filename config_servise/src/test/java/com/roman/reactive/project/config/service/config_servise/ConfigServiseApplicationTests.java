1b09e92f63d5c6df97076cc5c7ef9edb2cc9e10e
Roman <97985238+Roman-Chehovskiy@users.noreply.github.com>
2023-04-29 00:18:54 +0800

    Add files via upload

---
+++ config_servise/src/test/java/com/roman/reactive/project/config/service/config_servise/ConfigServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.reactive.project.config.service.config_servise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
