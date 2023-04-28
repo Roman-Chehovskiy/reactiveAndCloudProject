72d2000efe0f29089a5893dc9f0ddbe0e5081d92
Roman <97985238+Roman-Chehovskiy@users.noreply.github.com>
2023-04-29 00:23:06 +0800

    add eureka service

---
+++ reactive-eureka-server/reactive-eureka-server/src/test/java/com/roman_chechovskij/project/reactive/eureka/server/reactiveeurekaserver/ReactiveEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman_chechovskij.project.reactive.eureka.server.reactiveeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
