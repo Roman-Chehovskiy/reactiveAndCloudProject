7535a1b677239368c6022cf80d205205c9bc7af7
Roman <97985238+Roman-Chehovskiy@users.noreply.github.com>
2023-04-29 00:24:10 +0800

    add gateway service

---
+++ reactive_gateway/src/main/java/com/roman/reactive/project/gateway/reactive_gateway/ReactiveGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.roman.reactive.project.gateway.reactive_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReactiveGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveGatewayApplication.class, args);
+	}
+
+}
